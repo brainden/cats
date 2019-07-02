fe578f78bc4546c6919d24e5755645410694a35a
Brandon <brandonkilic@gmail.com>
2019-07-02 23:43:59 +0000

    cats

---
+++ test/syncBreedsTest.js
@@ -0,0 +1,20 @@
+const assert = require('chai').assert;
+const breedDetails = require('../syncBreeds');
+
+describe('#breedDetails', () => {
+  it('returns breed details for the Bombay breed', () => {
+    const breedDetails = function(breed) {
+        // can simply return it (easy peezee, butter squeezy) ...
+        return catBreeds[breed];
+      }
+      
+    console.log(breedDetails);
+  }),
+
+  it('return breed deatils for the Balinese breed', () => {
+    const breedDetails = function(breed) {
+        // can simply return it (easy peezee, butter squeezy) ...
+    return catBreeds[breed];
+  }
+})
+});
